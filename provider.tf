57b3247df08258ed9f0e59c018cb8b8e07c549a1
Deepak Kumar Pradhan <90602410+bsddv532@users.noreply.github.com>
2023-06-11 17:27:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -19,6 +19,6 @@ provider "aws" {
 # Configure the New Relic provider
 provider "newrelic" {
   account_id = 3628623
-  api_key    = NRAK-HS818W5YYIUP2OI9TB4CJ0RVGEN" # Usually prefixed with 'NRAK'
+  api_key    = "NRAK-HS818W5YYIUP2OI9TB4CJ0RVGEN" # Usually prefixed with 'NRAK'
   region     = "US"                               # Valid regions are US and EU
 }
